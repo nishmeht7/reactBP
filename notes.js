7103585c8b9866a787521425f8b0409a933b817f
Nishant Mehta <nishmeht7@gmail.com>
2017-06-24 18:23:12 -0500

    index.html working

--- notes.js
+++ notes.js
@@ -1,4 +1,12 @@
 /*
 Started with initializing package.json - npm init
 
+
+
+
+Difference between transpiler and compiler? 
+compiler compiles code like a C++ compiler takes C++ and compiles it into machine
+readible code (non-human readible)
+While transpiler transpiles code into human readible code for example webpack!!
+another example: node-sass transpiler takes scss and transpiles it to css
 */
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 
+router.get('/home', function(req, res){
+	res.send('Welcome to the world of heroNish!')
+})
+
+
 module.exports = router;
