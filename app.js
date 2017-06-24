3c73cd95adb6b4f632e2a4d627a002e6271923bd
Nishant Mehta <nishmeht7@gmail.com>
2017-06-24 18:05:50 -0500

    server set up

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const apiRoutes = require('./routes/index.js');
+const path = require('path');
+const router = require('express').Router();
 
 //morgan middleware
 app.use(morgan('dev'));
@@ -15,3 +17,14 @@ app.use('/api', apiRoutes);
 
 //posting all files in public folder
 app.use(express.static('public'));
+
+//all other routes
+app.get('/*', function(req, res){
+	res.sendFile(path.resolve(__dirname, 'browser/index.html'));
+})
+
+//listening on port 3005
+const server = app.listen(3005, function(){
+		console.log('listening on 3005');
+})
+
