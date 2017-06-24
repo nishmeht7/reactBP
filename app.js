e18cf5e5b548af7fb3e6a71a8a725cc4e1298a09
Nishant Mehta <nishmeht7@gmail.com>
2017-06-24 16:16:33 -0500

    boiler plate plus public

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const apiRoutes = require('./routes/index.js');
+
+//morgan middleware
+app.use(morgan('dev'));
+
+//implementing body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
+
+//posting all files in public folder
+app.use(express.static('public'));
